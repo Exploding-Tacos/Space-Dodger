d1c2d6346cdccacee027d145e3a867c40d54d34c
Sam Baldwin <ceo@adhdsam.com>
2024-05-16 17:12:12 +0100

    adds Better Background and attempted multi bullet(is commented out)

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 import pygame
 from sys import exit
+import random
 
 pygame.init()
-screen = pygame.display.set_mode((900,600))
+screen = pygame.display.set_mode((1200,900))
 pygame.display.set_caption("Tutorial")
 
 clock = pygame.time.Clock()
@@ -11,8 +12,7 @@ Mainmenu_font = pygame.font.Font(None, 50)
 score = 0
 player_grav = 0
 
-sky = pygame.image.load("Graphics/sky.png").convert_alpha()
-floor = pygame.image.load("Graphics/floor.png").convert_alpha()
+sky = pygame.image.load("Graphics/space.png").convert_alpha()
 
 name_surf = Mainmenu_font.render('Space Dodgers', True, (255, 255, 255))
 name_rect = name_surf.get_rect(center = (450, 50))
@@ -20,11 +20,11 @@ name_rect = name_surf.get_rect(center = (450, 50))
 score_surf = Mainmenu_font.render(f'Score: {score}', True, (255, 255, 255))
 score_rect = name_surf.get_rect(center = (450, 150))
 
-Bullet_surface = pygame.image.load('Graphics/Bullet.png').convert_alpha()
-Bullet_rect = Bullet_surface.get_rect(midbottom = (900, 350))
-
 player_surf = pygame.image.load("Graphics/Playership.png").convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (150, 350))
+
+Bullet_surface = pygame.image.load('Graphics/Bullet.png').convert_alpha()
+Bullet_rect = Bullet_surface.get_rect(midbottom = (900, 350))
 
 while True:
 
@@ -36,8 +36,7 @@ while True:
 			pygame.quit()
 			exit()
 
-	screen.blit(sky, (0,0))
-	screen.blit(floor, (0,350))
+	screen.blit(sky, (0,0))	
 	pygame.draw.rect(screen,(155,0,22),name_rect)
 	pygame.draw.rect(screen,(155,0,22),name_rect,10)
 	screen.blit(name_surf, name_rect)
@@ -45,13 +44,25 @@ while True:
 	pygame.draw.rect(screen,(155,0,22),score_rect)
 	pygame.draw.rect(screen,(155,0,22),score_rect,10)
 	screen.blit(score_surf, score_rect)
+	screen.blit(Bullet_surface, Bullet_rect)
 
-	#Bullet
+	'''placinglist = ["1", "2", "3"]
+	placinglit = random.choice(placinglist)
+	if placinglit == "1":
+		placingbullet = 350
+	elif placinglit == "2":
+		placingbullet = 450
+	elif placinglit == "3":
+		placingbullet = 550
+
+	Bullet_surface = pygame.image.load('Graphics/Bullet.png').convert_alpha()
+	Bullet_rect = Bullet_surface.get_rect(midbottom = (900, placingbullet))'''
+
 	Bullet_rect.x -= 6
 	if Bullet_rect.right <= 0:
 		Bullet_rect.left = 900
 		score += 1
-	screen.blit(Bullet_surface,Bullet_rect)
+		print("bol")
 
 	#Player
 	player_grav += 1
