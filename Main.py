bb482f1572ba8d11dea3d32c1cafb226326fed61
Sam Baldwin <ceo@adhdsam.com>
2024-05-22 19:26:16 +0100

    Improved menu

--- Main.py
+++ Main.py
@@ -3,90 +3,77 @@ from sys import exit
 import random
 from pygame import mixer 
 
-pygame.init()
-screen = pygame.display.set_mode((1200,900))
-pygame.display.set_caption("Space Dodgers")
-Icon = pygame.image.load('Graphics/Playership.png')
-pygame.display.set_icon(Icon)
-
-clock = pygame.time.Clock()
-Mainmenu_font = pygame.font.Font(None, 50)
-
-score = 0
-player_grav = 0
-placingbulle = 485
-
-space = pygame.image.load("Graphics/space.png").convert_alpha()
-
-name_surf = Mainmenu_font.render('Space Dodgers', True, (255, 255, 255))
-name_rect = name_surf.get_rect(center = (450, 50))
-
-score_surf = Mainmenu_font.render(f'Score: {score}', True, (255, 255, 255))
-score_rect = name_surf.get_rect(center = (450, 150))
-
-player_surf = pygame.image.load("Graphics/Playership.png").convert_alpha()
-player_rect = player_surf.get_rect(midbottom = (150, 470))
-
-Bullet_surface = pygame.image.load('Graphics/Bullet.png').convert_alpha()
-Bullet_rect = Bullet_surface.get_rect(midbottom = (960, placingbulle))
-
-mixer.init() 
-mixer.music.load("Music/GameMusic.mp3") 
-mixer.music.set_volume(0.7) 
-musicrun = True
-pygame.mixer.music.play(loops=-1, start=0.0, fade_ms=0)
-
-while True:
-
+def Playfunc():
+	pygame.init()
+	screen = pygame.display.set_mode((1200,900))
+	pygame.display.set_caption("Space Dodgers")
+	Icon = pygame.image.load('Graphics/Playership.png')
+	pygame.display.set_icon(Icon)
+	clock = pygame.time.Clock()
+	Mainmenu_font = pygame.font.Font(None, 50)
+	score = 0
+	player_grav = 0
+	placingbulle = 485
+	space = pygame.image.load("Graphics/space.png").convert_alpha()
+	name_surf = Mainmenu_font.render('Space Dodgers', True, (255, 255, 255))
+	name_rect = name_surf.get_rect(center = (450, 50))
 	score_surf = Mainmenu_font.render(f'Score: {score}', True, (255, 255, 255))
 	score_rect = name_surf.get_rect(center = (450, 150))
+	player_surf = pygame.image.load("Graphics/Playership.png").convert_alpha()
+	player_rect = player_surf.get_rect(midbottom = (150, 470))
+	Bullet_surface = pygame.image.load('Graphics/Bullet.png').convert_alpha()
+	Bullet_rect = Bullet_surface.get_rect(midbottom = (960, placingbulle))
+	
+	while True:
+	
+		score_surf = Mainmenu_font.render(f'Score: {score}', True, (255, 255, 255))
+		score_rect = name_surf.get_rect(center = (450, 150))
+	
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				exit()
+	
+		screen.blit(space, (0,0))	
+		pygame.draw.rect(screen,(0,0,0),name_rect)
+		pygame.draw.rect(screen,(255,255,255),name_rect,2)
+		screen.blit(name_surf, name_rect)
+	
+		pygame.draw.rect(screen,(0,0,0),score_rect)
+		pygame.draw.rect(screen,(255,255,255),score_rect,2)
+		screen.blit(score_surf, score_rect)
+		screen.blit(Bullet_surface, Bullet_rect)
 
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			exit()
+		placinglist = ["1", "2", "3"]
+		placinglit = random.choice(placinglist)
+		if placinglit == "1":
+			placingbullet = 340
+		elif placinglit == "2":
+			placingbullet = 485
+		elif placinglit == "3":
+			placingbullet = 600
+	
+		Bullet_rect.x -= 6
+		if Bullet_rect.right <= 0:
+			Bullet_rect.left = 960
+			score += 1
+			placingbulle = placingbullet
+			Bullet_surface = pygame.image.load('Graphics/Bullet.png').convert_alpha()
+			Bullet_rect = Bullet_surface.get_rect(midbottom = (960, placingbulle))
+	
+	
+		#Player
+		player_grav += 1
+		player_rect.y += player_grav
+		screen.blit(player_surf,player_rect)
+	
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_SPACE]:
+			player_grav = -20
+	
+		Collod = player_rect.colliderect(Bullet_rect)
+		if Collod == True:
+			score = 0
 
-	screen.blit(space, (0,0))	
-	pygame.draw.rect(screen,(0,0,0),name_rect)
-	pygame.draw.rect(screen,(255,255,255),name_rect,2)
-	screen.blit(name_surf, name_rect)
-
-	pygame.draw.rect(screen,(0,0,0),score_rect)
-	pygame.draw.rect(screen,(255,255,255),score_rect,2)
-	screen.blit(score_surf, score_rect)
-	screen.blit(Bullet_surface, Bullet_rect)
-
-	placinglist = ["1", "2", "3"]
-	placinglit = random.choice(placinglist)
-	if placinglit == "1":
-		placingbullet = 340
-	elif placinglit == "2":
-		placingbullet = 485
-	elif placinglit == "3":
-		placingbullet = 600
-
-	Bullet_rect.x -= 6
-	if Bullet_rect.right <= 0:
-		Bullet_rect.left = 960
-		score += 1
-		placingbulle = placingbullet
-		Bullet_surface = pygame.image.load('Graphics/Bullet.png').convert_alpha()
-		Bullet_rect = Bullet_surface.get_rect(midbottom = (960, placingbulle))
-
-
-	#Player
-	player_grav += 1
-	player_rect.y += player_grav
-	screen.blit(player_surf,player_rect)
-
-	keys = pygame.key.get_pressed()
-	if keys[pygame.K_SPACE]:
-		player_grav = -20
-
-	Collod = player_rect.colliderect(Bullet_rect)
-	if Collod == True:
-		score = 0
-		#print("Collision")
-
-	pygame.display.update()
-	clock.tick(60)
+		pygame.display.update()
+		clock.tick(60)
--- Menu.py
+++ Menu.py
@@ -1,3 +1,4 @@
+import Main
 import pygame
 from sys import exit
 import random
@@ -8,27 +9,39 @@ screen = pygame.display.set_mode((1200,900))
 pygame.display.set_caption("Space Dodgers")
 Icon = pygame.image.load('Graphics/Playership.png')
 pygame.display.set_icon(Icon)
+Mainmenu_font = pygame.font.Font(None, 50)
 clock = pygame.time.Clock()
 font = pygame.font.Font(None, 50)
 button_surface = pygame.Surface((150, 50))
 text = font.render("Play", True, (0, 0, 0))
 text_rect = text.get_rect(center=(button_surface.get_width()/2, button_surface.get_height()/2))
-button_rect = pygame.Rect(125, 125, 150, 50)  # Adjust the position as needed
+button_rect = pygame.Rect(500, 450, 150, 50)  # Adjust the position as needed
+name_surf = Mainmenu_font.render('Space Dodgers', True, (255, 255, 255))
+name_rect = name_surf.get_rect(center = (600, 300))
+mixer.init() 
+mixer.music.load("Music/GameMusic.mp3") 
+mixer.music.set_volume(0.7) 
+musicrun = True
+pygame.mixer.music.play(loops=-1, start=0.0, fade_ms=0)
 
 while True:
 
     clock.tick(60)
  
-    screen.fill((155, 255, 155))
+    screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+ 
+    pygame.draw.rect(screen,(0,0,0),name_rect)
+    pygame.draw.rect(screen,(255,255,255),name_rect,2)
+    screen.blit(name_surf, name_rect)
 
     if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
         if button_rect.collidepoint(event.pos):
-            print("Button clicked!")
+            Main.Playfunc()
 
     if button_rect.collidepoint(pygame.mouse.get_pos()):
         pygame.draw.rect(button_surface, (127, 255, 212), (1, 1, 148, 48))
